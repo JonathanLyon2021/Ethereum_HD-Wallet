0cf100bcaffc25c9a58a3d3335879f153ca90ae5
Jonathan Lyon <jessicacrawfordoptimus010@gmail.com>
2022-04-25 15:23:50 -0400

    Sign transaction(values must be input to run program)

--- HD_Wallet.js
+++ HD_Wallet.js
@@ -73,27 +73,49 @@
 //#6 Derive Keys from HD Wallet //////////////////////////////////
 //Derive Keys and addresses from HD Wallet by Ethereum's derivation path//
 
+// const ethers = require('ethers');
+
+// function deriveFiveWalletsFromHdNode(mnemonic, derivationPath) {
+//     let wallets = []
+
+//     for(let index = 0; index < 5; index ++) {
+//         let hdNode = ethers.utils.HDNode.fromMnemonic(mnemonic).derivePath(
+//             derivationPath + index,
+//         )
+//         console.log('Derivation path', hdNode.path)
+//         console.log('Address', hdNode.address)
+//         console.log('Private key', hdNode.privateKey)
+//         console.log('==========')
+//         let wallet = new ethers.Wallet(hdNode.privateKey)
+//         wallets.push(wallet)
+//     }
+//     return wallets;
+// }
+
+// let mnemonic = 
+// 'upset fuel enhance depart portion hope core animal innocent will athlete snack'
+// let derivationPath = "m/44'/60'/0'/0/"
+
+// deriveFiveWalletsFromHdNode(mnemonic, derivationPath)
+
+//#7 Sign a Transaction//
+//From the derived wallets, take the second wallet and sign a transaction//
+
 const ethers = require('ethers');
 
-function deriveFiveWalletsFromHdNode(mnemonic, derivationPath) {
-    let wallets = []
-
-    for(let index = 0; index < 5; index ++) {
-        let hdNode = ethers.utils.HDNode.fromMnemonic(mnemonic).derivePath(
-            derivationPath + index,
-        )
-        console.log('Derivation path', hdNode.path)
-        console.log('Address', hdNode.address)
-        console.log('Private key', hdNode.privateKey)
-        console.log('==========')
-        let wallet = new ethers.Wallet(hdNode.privateKey)
-        wallets.push(wallet)
+async function signTransaction(wallet, toAddress, value) {
+    let transaction = {
+        nonce: 0,
+        gasLimit: 21000,
+        gasPrice: ethers.BigNumber.from('0x1'),
+        to: toAddress,
+        value: ethers.utils.parseEther(value),
+        data: '0x',
     }
-    return wallets;
+    const signedTransaction = await wallet.signTransaction(transaction);
+    console.log("Signed Transaction:")
+    console.log(signedTransaction)
+    return signedTransaction;
 }
 
-let mnemonic = 
-'upset fuel enhance depart portion hope core animal innocent will athlete snack'
-let derivationPath = "m/44'/60'/0'/0/"
-
-deriveFiveWalletsFromHdNode(mnemonic, derivationPath)
+signTransaction();
