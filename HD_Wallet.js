266c68f818d67f41da64ed6301bae090e5151470
Jonathan Lyon <jessicacrawfordoptimus010@gmail.com>
2022-04-25 13:11:21 -0400

    Ethers.Wallet instance, also added notes

--- HD_Wallet.js
+++ HD_Wallet.js
@@ -1,10 +1,23 @@
+//#1 //Restoring HD Wallet by Existing Mnemonic// 
+// const ethers = require('ethers');
+
+// function restoreHDWallet(mnemonic){
+//     return ethers.utils.HDNode.fromMnemonic(mnemonic)
+// }
+
+// let mnemonic = 
+//     'upset fuel enhance depart portion hope core animal innocent will athlete snack'
+//     console.log(restoreHDWallet(mnemonic))
+
+//#2 //ethers.Wallet instance actually uses HDNode.fromMnemonic, derives once, 
+//and from the new HD node, it takes the private key to build the wallet.//
 const ethers = require('ethers');
 
-function restoreHDWallet(mnemonic){
-    return ethers.utils.HDNode.fromMnemonic(mnemonic)
+function restoreHDWallet(mnemonic) {
+    return ethers.Wallet.fromMnemonic(mnemonic)
 }
 
 let mnemonic = 
     'upset fuel enhance depart portion hope core animal innocent will athlete snack'
-    console.log(restoreHDWallet(mnemonic))
-    
+console.log(restoreHDWallet(mnemonic));
+
