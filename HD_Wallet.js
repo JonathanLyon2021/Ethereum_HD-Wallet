1386222617a876ecc2d1d8e68bfba5f8c03d1da2
Jonathan Lyon <jessicacrawfordoptimus010@gmail.com>
2022-04-25 14:56:15 -0400

    Derive Keys from HD Wallet

--- HD_Wallet.js
+++ HD_Wallet.js
@@ -46,27 +46,54 @@
 //#5 Load HD Wallet from JSON -
 // Decrypt and load an HD node from a JSON wallet using a password//
 
+// const ethers = require('ethers');
+
+// async function decryptWallet(json, password) {
+//     return ethers.Wallet.fromEncryptedJson(json, password)
+// }
+
+// async function createAndSaveWalletAsJSON(password) {
+//     const wallet = await ethers.Wallet.createRandom()
+//     console.log('Wallet before encryption ===')
+//     console.log(wallet)
+//     return wallet.encrypt(password)
+// }
+
+// ;(async () => {
+//     const password = 'Kingsland University';
+//     //Encryption
+//     let json = await createAndSaveWalletAsJSON(password)
+
+//     //Decryption
+//     let walletDecrypted = await decryptWallet(json, password)
+//     console.log('Wallet after decryption ===')
+//     console.log(walletDecrypted)
+// })()
+
+//#6 Derive Keys from HD Wallet //////////////////////////////////
+//Derive Keys and addresses from HD Wallet by Ethereum's derivation path//
+
 const ethers = require('ethers');
 
-async function decryptWallet(json, password) {
-    return ethers.Wallet.fromEncryptedJson(json, password)
+function deriveFiveWalletsFromHdNode(mnemonic, derivationPath) {
+    let wallets = []
+
+    for(let index = 0; index < 5; index ++) {
+        let hdNode = ethers.utils.HDNode.fromMnemonic(mnemonic).derivePath(
+            derivationPath + index,
+        )
+        console.log('Derivation path', hdNode.path)
+        console.log('Address', hdNode.address)
+        console.log('Private key', hdNode.privateKey)
+        console.log('==========')
+        let wallet = new ethers.Wallet(hdNode.privateKey)
+        wallets.push(wallet)
+    }
+    return wallets;
 }
 
-async function createAndSaveWalletAsJSON(password) {
-    const wallet = await ethers.Wallet.createRandom()
-    console.log('Wallet before encryption ===')
-    console.log(wallet)
-    return wallet.encrypt(password)
-}
+let mnemonic = 
+'upset fuel enhance depart portion hope core animal innocent will athlete snack'
+let derivationPath = "m/44'/60'/0'/0/"
 
-;(async () => {
-    const password = 'Kingsland University';
-    //Encryption
-    let json = await createAndSaveWalletAsJSON(password)
-
-    //Decryption
-    let walletDecrypted = await decryptWallet(json, password)
-    console.log('Wallet after decryption ===')
-    console.log(walletDecrypted)
-})()
-
+deriveFiveWalletsFromHdNode(mnemonic, derivationPath)
